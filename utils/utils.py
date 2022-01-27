5c1ead45945490981f296c036ccbda9f743ad579
wjx1198 <41059906+wjx1198@users.noreply.github.com>
2022-01-27 11:15:43 +0800

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -16,31 +16,6 @@ from torch.nn.modules.loss import _Loss
 import pdb
 
 
-class SoftIoULoss(nn.Module):
-    def __init__(self, n_classes):
-        super(SoftIoULoss, self).__init__()
-        self.n_classes = n_classes
-
-    def forward(self, input, target):
-        # logit => N x Classes x H x W
-        # target => N x H x W
-        N = len(input)
-        pred = F.softmax(input, dim=1)
-        target_onehot = target
-        # target_onehot = self.to_one_hot(target, self.n_classes)
-        # Numerator Product
-        inter = pred * target_onehot
-        # Sum over all pixels N x C x H x W => N x C
-        inter = inter.view(N, self.n_classes, -1).sum(2)
-        # Denominator
-        union = pred + target_onehot - (pred * target_onehot)
-        # Sum over all pixels N x C x H x W => N x C
-        union = union.view(N, self.n_classes, -1).sum(2)
-        loss = inter / (union + 1e-16)
-        # Return average loss over classes and batch
-        return -loss.mean()
-
-
 def prepare_data(dataset_dir):
     train_input_names = []
     train_output_names = []
